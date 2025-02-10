e819e8abe7638ebd940c43e024c196ac72670957
Annalisac8 <annalisac8@gmail.com>
2025-02-10 15:34:52 +0100

    definita funzione parallela per inizializzare il filtro

--- parallel_BloomFilter.py
+++ parallel_BloomFilter.py
@@ -3,8 +3,7 @@ import hashlib
 
 def calcola_hash(stringa, n, j):
     sha256 = hashlib.sha256()
-    # Aggiorna l'oggetto sha256 con la stringa e l'indice j
-    sha256.update((stringa + str(j)).encode('utf-8'))
+    sha256.update((stringa + str(j)).encode('utf-8'))  # Aggiorna con la stringa e l'indice j
     return int(sha256.hexdigest(), 16) % n
 
 class BloomFilterParallelo:
@@ -14,3 +13,16 @@ class BloomFilterParallelo:
         self.dimensione = dimensione
         self.numero_hash = numero_hash
         self.numero_thread = numero_thread
+
+    def inizializza(self, url):
+        risultato = []  # lista  per contenere i risultati
+        with Parallel(n_jobs=self.numero_thread) as parallelo:  # Riutilizza lo stesso pool di thread
+            for j in range(self.numero_hash): # Itero per il numero di volte specificato in numero_hash
+                # Parallelizza applicando l'hash a più elementi della lista di email
+                # delayed: aspetta che calcola_hash venga fatto su ogni elemento della lista prima di passare all'hash successivo
+                risultato.extend(parallelo(delayed(calcola_hash)(url[i], self.dimensione, j) for i in range(len(url))))
+
+        # Itero per ogni elemento in risultato
+        for i in range(len(risultato)):
+            # Imposto l'elemento corrispondente in array_bit a 1
+            self.array_bit[risultato[i]] = 1
