4dfaa0a61cb2ce18b143bb59da5bbdcaeb076861
Annalisac8 <annalisac8@gmail.com>
2025-02-10 15:42:23 +0100

    aggiutna funzione per verifica degli elementi come nel caso sequenziale. acora da parallelizzare la verifica. + refactoring di alcune parti del codice.

--- parallel_BloomFilter.py
+++ parallel_BloomFilter.py
@@ -14,15 +14,20 @@ class BloomFilterParallelo:
         self.numero_hash = numero_hash
         self.numero_thread = numero_thread
 
-    def inizializza(self, url):
+    def inizializza(self, elemento):
         risultato = []  # lista  per contenere i risultati
         with Parallel(n_jobs=self.numero_thread) as parallelo:  # Riutilizza lo stesso pool di thread
             for j in range(self.numero_hash): # Itero per il numero di volte specificato in numero_hash
                 # Parallelizza applicando l'hash a più elementi della lista di email
                 # delayed: aspetta che calcola_hash venga fatto su ogni elemento della lista prima di passare all'hash successivo
-                risultato.extend(parallelo(delayed(calcola_hash)(url[i], self.dimensione, j) for i in range(len(url))))
+                risultato.extend(parallelo(delayed(calcola_hash)(elemento[i], self.dimensione, j) for i in range(len(elemento))))
 
         # Itero per ogni elemento in risultato
         for i in range(len(risultato)):
             # Imposto l'elemento corrispondente in array_bit a 1
             self.array_bit[risultato[i]] = 1
+    def verifica(self, elemento): # Funzione per verificare
+        for i in range(self.numero_hash):
+            if self.array_bit[calcola_hash(elemento, self.dimensione, i)] == 0:
+                return False # Se uno dei bit è 0, l'elemento sicuramente non è presente
+        return True # Potrebbe essere presente (ma esistono falsi positivi)
